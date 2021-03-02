87e6223112066de0ac84efca0183ad3ffda50788
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-02 23:45:52 +1000

    Create forLoop.js

---
+++ Wk01_Intro/LanguageBasics/Loops/ForLoop/forLoop.js
@@ -0,0 +1,6 @@
+//Counter controlled loop - displays 1 - 100
+
+//starting at 1, counting towards 100. At the end of the loop, increment value of count 
+for (let count = 1; count <= 100; count++) {
+  console.log(count); //display the new value of count
+}
