5f1056079d164bbba8561e740b9e6d2e12c42886
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-03 00:24:33 +1000

    Create compoundAssignment.js

---
+++ Wk01_Intro/Operators&Assignments/CompoundAssignment/compoundAssignment.js
@@ -0,0 +1,19 @@
+// Initial values
+let a = 10, b = 5;
+
+a+=b; // same as [a = a + b]
+console.log("a+=b: " + a);
+
+// Restored
+a-=b; // same as [a = a - b]
+console.log("a-=b: " + a);
+
+a*=b; // same as [a = a * b]
+console.log("a*=b: " + a);
+
+// Restored
+a/=b; // same as [a = a * b]
+console.log("a/=b: " + a);
+
+a%=b; // same as [a = a % b]
+console.log("a%=b: " + a);
