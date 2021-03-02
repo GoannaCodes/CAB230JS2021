09a81e9bef371699654a99fa7cd19ac6bc405fb8
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-02 23:56:18 +1000

    Create PretestLoop.js

---
+++ Wk01_Intro/LanguageBasics/Loops/WhileLoop.js/PretestLoop.js
@@ -0,0 +1,6 @@
+/* Pre-tested loop using while loop */
+let count = 1; //counter declaration
+while (count <= 100) { //condition to execute while loop i.e while count is less than or equal to 100
+  console.log(count); //display current value of count
+  count++; //increment count, so that a new number is tested
+}
