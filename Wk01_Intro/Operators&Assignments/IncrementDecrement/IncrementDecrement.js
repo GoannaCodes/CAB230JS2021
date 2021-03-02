07cd493d5dceaef2c8d889f122d0f4316ec6cbff
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-03 00:21:35 +1000

    Create IncrementDecrement.js

---
+++ Wk01_Intro/Operators&Assignments/IncrementDecrement/IncrementDecrement.js
@@ -0,0 +1,17 @@
+let a = 3 + 4; //addition 7
+console.log("a: " + a);
+
+let b = 7 - 4; //subtraction 3
+console.log("b: " + b);
+
+let c = b++; //post-increment 3
+console.log("c: " + c);
+
+let d = c--; //post-decrement 3
+console.log("d: " + d);
+
+let e = ++b; //pre-increment 5
+console.log("e: " + e);
+
+let f = --e; //pre-decrement 4
+console.log("f: " + f);
