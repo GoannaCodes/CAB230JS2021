e8a323e701d735c8dac022d41c244e2ed6c4c5f2
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-02 23:59:46 +1000

    Create PosttestLoop.js

---
+++ Wk01_Intro/LanguageBasics/Loops/WhileLoop.js/PosttestLoop.js
@@ -0,0 +1,11 @@
+/* Post-tested Loop using do-while loop */
+//counter declaration/initialisation
+let count = 0; 
+
+//execute the following code once
+do { 
+  //increment counter value
+  count++; 
+  //display current value of count
+  console.log(count); 
+} while (count < 100); //if condition is still true, iterate loop again
