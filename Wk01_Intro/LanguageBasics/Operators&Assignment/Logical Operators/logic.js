543453805c095acafd28f5c2973ce5e0cb4b7ed5
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-08 18:18:27 +1000

    Update logic.js

--- Wk01_Intro/LanguageBasics/Operators&Assignment/Logical Operators/logic.js
+++ Wk01_Intro/LanguageBasics/Operators&Assignment/Logical Operators/logic.js
@@ -1,12 +1,1 @@
-// Initial values 
-let x = 10; y = 20;
 
-//&& - Logical AND 
-(x < y) && (y == 20); // true
-(x < y) && (y > 20); // false
-(x >= 10) && !(y == 200); //true 
-
-//|| - Logical OR
-(x < y) || (y == 20); // true
-(x < y) || (y > 20); // true
-(x < 10) || !(y == 200); //true 
