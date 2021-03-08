64fbd62ceac2ec46801e8a1b1e2aac182503f65c
GoannaCodes <78517290+GoannaCodes@users.noreply.github.com>
2021-03-08 18:15:12 +1000

    Update addingStrings.js

--- Wk01_Intro/Operators&Assignments/CompoundAssignment/addingStrings.js
+++ Wk01_Intro/Operators&Assignments/CompoundAssignment/addingStrings.js
@@ -1,11 +1,1 @@
-//Initial values
-let hello = "Hello World";
-let again = "Hello Cruel World";
 
-//Concats hello and again; doesn't affect string stored in hello
-let text = hello + " , " + again;
-console.log(text); //displays Hello World, Hello Cruel World
-
-//Appends onto hello variable "," and string stored in again; changes overall string of hello
-hello += ", " + again;
-console.log(hello); //displays Hello World, Hello Cruel World
